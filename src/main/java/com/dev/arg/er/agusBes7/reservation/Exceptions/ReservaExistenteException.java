b47ab36ebf7df4a85e63895735c12ba4676e53e6
agusBes7 <aguscabj10.ab@gmail.com>
2026-03-24 19:07:48 -0300

    aplicando modificaciones

--- src/main/java/com/dev/arg/er/agusBes7/reservation/Exceptions/ReservaExistenteException.java
+++
@@ -1,8 +0,0 @@
-package com.dev.arg.er.agusBes7.reservation.Exceptions;
-
-public class ReservaExistenteException extends RuntimeException {
-
-    public ReservaExistenteException(String mensaje) {
-        super(mensaje);
-    }
-}
